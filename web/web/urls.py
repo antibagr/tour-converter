b69c194d573cbc63fc82266b4248a59ea2a2d5be
Linode Rudie <antibagr@yandex.ru>
2021-04-18 19:42:08 +0000

    Update docker-compose.yml at production

--- web/web/urls.py
+++ web/web/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
+if False: # settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
